c187cee74735496ac57ab1e690b75160d0b093b7
Michele Mammini <mamminim@gmail.com>
2013-07-10 16:37:24 +0200

    update field to be added in dbmng_fields

--- sql/dbmng_2.sql
+++ sql/dbmng_2.sql
@@ -1,2 +1,4 @@
 ALTER TABLE  dbmng_fields ADD  field_label_long VARCHAR( 100 ) NOT NULL AFTER  field_label;
 ALTER TABLE  dbmng_fields ADD  skip_in_tbl INT DEFAULT 0;
+
+ALTER TABLE  dbmng_fields ADD  voc_sql VARCHAR ( 255 ) DEFAULT NULL;
