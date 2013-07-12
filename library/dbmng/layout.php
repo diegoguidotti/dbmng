2e145b47d6203e2d9cc3e6bb979ba4ab3d48b778
Michele Mammini <mamminim@gmail.com>
2013-07-12 16:41:28 +0200

    correct bug on layout_form_textarea

--- library/dbmng/layout.php
+++ library/dbmng/layout.php
@@ -37,7 +37,7 @@ function layout_form_textarea( $fld, $fld_value, $value )
 	$html  = "";
 	$html .= "<label for='$fld'>" . t($fld_value['label_long']) . "</label>\n";
 	$html .= "<textarea  name='$fld' id='$fld'  $other >";
-	$html .= " $value ";	
+	$html .= $value;	
 	$html .= "</textarea>\n";
 	return $html;
 }
