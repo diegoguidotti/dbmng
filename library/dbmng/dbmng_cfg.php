7d4d73551da3f5b9b0edf54502e4a069faa3e79d
diegoguidotti <diego.guidotti@gmail.com>
2013-07-15 21:30:51 +0200

    solved a push problem

--- library/dbmng/dbmng_cfg.php
+++ library/dbmng/dbmng_cfg.php
@@ -1,5 +1,4 @@
 <?php
-<<<<<<< HEAD
 
     if(!defined( 'DBMNG_CMS'))
 			{
@@ -11,10 +10,8 @@
 				define( 'DBMNG_DB'           , 'mysql' );   // available value: 'mysql', 'postgres', 'pdo'
 			}
 
-=======
 		define( 'DBMNG_CMS'          , 'drupal' ); // available value: 'drupal', 'joomla', 'none'
 		define( 'DBMNG_DB'           , 'mysql' ); // available value: 'mysql', 'mysqli', 'postgres', 'pdo'
->>>>>>> 172ebb0aa244091cce94b7edd691796d72be2a70
 		define( 'DBMNG_DB_HOST'      , 'localhost' );
 		define( 'DBMNG_DB_NAME'      , 'clima' );
 		define( 'DBMNG_DB_USER'      , 'admin' );
