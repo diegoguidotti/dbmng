1c0314392778c4de4292129f87e632c53390e483
diegoguidotti <diego.guidotti@gmail.com>
2013-07-18 19:03:10 +0200

    Added jQuery Library in the standalone test file

--- tests/test.php
+++ tests/test.php
@@ -1,5 +1,12 @@
 <html>
 <head>
+
+<!-- Load jQuery Library -->
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
+<link rel="stylesheet" type="text/css" href="http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css" />
+
+<!-- Load DBMG JS&CSS -->
 <script type="text/javascript" src="../library/dbmng/assets/dbmng.js?mpvqml"></script>
 <style type="text/css" media="all">
 	@import url("../library/dbmng/assets/dbmng.css");
@@ -18,19 +25,10 @@
 	include(DBMNG_LIB_PATH.'dbmng.php');
 	include(DBMNG_LIB_PATH.'dbmng_standalone.php');
 
-
-	$rs=dbmng_query("select * from test");
-	foreach($rs as $r){
-		//print_r($r);
-		//echo('_'.$r->id_test.'_<br/>');
-	}
-
-
 	//get the array storing the table metadata from record 1 in table dbmng_tables
 	$aForm    = dbmng_get_form_array(11); 
-	$aParam   = array();                 //associative array storing custom parameters
 
-	echo dbmng_crud($aForm, $aParam);
+	echo dbmng_crud($aForm);
 
 /*
 	$aForm=array(  
