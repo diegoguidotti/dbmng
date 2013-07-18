5f20137323a6da318a9bbb0a9f2c0a2c08c78813
Michele Mammini <mamminim@gmail.com>
2013-07-18 12:52:05 +0200

    updated dbmng_data2JSarray and dbmng_create_form

--- library/dbmng/dbmng.php
+++ library/dbmng/dbmng.php
@@ -230,66 +230,68 @@ function dbmng_create_table($aForm, $aParam)
 /////////////////////////////////////////////////////////////////////////////
 // dbmng_data2JSarray
 // ======================
-/// This function allow to have an array of data
+/// This function allow to define a JS array 
 /**
 \param $aForm  		Associative array with all the characteristics of the table
 \param $aParam  	Associative array with some custom variable used by the renderer
-\return           Array
+\return           html
 */
 function dbmng_data2JSarray($aForm, $aParam)
 {
-  
-  // Initialize where clause and hidden variables
-	$where = " WHERE 1 ";
-	$aData = array();
-
-		if(isset($aParam))
-			{
-				if( isset($aParam['filters']) )
+	if( !isset($_GET["upd_" . $aForm['table_name']]) &&  !isset($_GET["ins_" . $aForm['table_name']]) )
+	{
+	  // Initialize where clause and hidden variables
+		$where = " WHERE 1 ";
+	
+			if(isset($aParam))
 				{
-					foreach ( $aParam['filters'] as $x => $x_value )
-						{				
-								$where.=" AND $x = $x_value ";
-						}					
-				}
-			}
-		
-		$html = "";
-		if( !isset($_GET["ins_" . $aForm['table_name']]) && !isset($_GET["upd_" . $aForm['table_name']]) )
-			{
-			  $sql = 'select * from ' . $aForm['table_name'].' '. $where;
-				$result = dbmng_query($sql);
-				
-				$AName = "_aTblVal";
-				$html .= "\n<script type='text/javascript'>\n";
-				$html .= "<!--\n";
-				$html .= "// $sql\n";
-				$html .= "$AName = {'records':[\n";
-				
-				foreach( $result as $record )
+					if( isset($aParam['filters']) )
 					{
-						$html .= "{";
-						//get the query results for each field
-						foreach ( $aForm['fields'] as $fld => $fld_value )
-							{
-								//echo '!'.$record->$fld.'!'.$fld.'<br/>';
-								$value=dbmng_value_prepare_html($fld_value, $record->$fld);
-								if( layout_view_field_table($fld_value) )
-									{
-										$html .= "'" . $fld . "':" . $value . ", ";
-										//$html .= "<td class='dbmng_field_$fld'>".$value."</td>";
-									}
-			
-							}
-						$html .= "}\n";
+						foreach ( $aParam['filters'] as $x => $x_value )
+							{				
+									$where.=" AND $x = $x_value ";
+							}					
 					}
+				}
+			
+			$html = "";
+			if( !isset($_GET["ins_" . $aForm['table_name']]) && !isset($_GET["upd_" . $aForm['table_name']]) )
+				{
+				  $sql = 'select * from ' . $aForm['table_name'].' '. $where;
+					$result = dbmng_query($sql);
+					
+					$AName = "_aTblVal";
+					$html .= "\n<script type='text/javascript'>\n";
+					$html .= "<!--\n";
+					$html .= "// $sql\n";
+					$html .= "$AName = {'records':[\n";
+					
+					$sObj  = "";
+					foreach( $result as $record )
+						{
+							$sObj .= "{";
+							//get the query results for each field
+							foreach ( $aForm['fields'] as $fld => $fld_value )
+								{
+									$value=dbmng_value_prepare_html($fld_value, $record->$fld);
+									if( layout_view_field_table($fld_value) )
+										{
+											$sObj .= "'" . $fld . "':" . $value . ", ";
+										}
 				
-				$html .= "]};";
-			}
-			$html .= "//-->\n";
-			$html .= "</script>\n";
-			return $html;
-	}
+								}
+							$sObj = substr($sObj, 0, strlen($sObj)-2);
+							$sObj .= "}, \n";
+						}
+					$sObj = substr($sObj, 0, strlen($sObj)-4);
+					
+					$html .= $sObj . "]};\n";
+				}
+				$html .= "//-->\n";
+				$html .= "</script>\n";
+				return $html;
+		}
+}
 /////////////////////////////////////////////////////////////////////////////
 // dbmng_create_form
 // ======================
@@ -333,9 +335,21 @@ function dbmng_create_form($aForm, $aParam)
 					$vals      = dbmng_fetch_object($result); //$result->fetchObject();
 					//print_r($vals);
 				}
+				
+			$more = "";
+			foreach ( $aForm['fields'] as $fld => $fld_value )
+				{
+					if( isset($fld_value['widget']) )
+						{
+							if( $fld_value['widget'] == 'file' )
+								{
+									$more = "enctype='multipart/form-data'";
+								}
+						}
+				}
 
 
-			$html .= "<div class='dbmng_form' id='dbmng_form_".$aForm['table_name']."' >\n<form method='POST' action='?' >\n".$hv."";
+			$html .= "<div class='dbmng_form' id='dbmng_form_".$aForm['table_name']."' >\n<form method='POST' $more action='?' >\n".$hv."";
 			foreach ( $aForm['fields'] as $fld => $fld_value )
 				{
 
