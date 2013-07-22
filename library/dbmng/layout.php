a0265622af78579c2d968408c68a1b94d69be53c
Michele Mammini <mamminim@gmail.com>
2013-07-22 14:35:57 +0200

    fine tuning of layout library

--- library/dbmng/layout.php
+++ library/dbmng/layout.php
@@ -56,7 +56,7 @@ function layout_get_label($fld, $fld_value)
 		
 		$sRequired = "";
 		if(isset($fld_value['nullable']) && $fld_value['nullable'] == 0)
-			$sRequired = "<div class='dbmng_required'>*</div>";
+			$sRequired = "<span class='dbmng_required'>*</span>";
 			
 		return "<label for='$fld'>" . t($lb) . $sRequired . "</label>\n";
 	}
