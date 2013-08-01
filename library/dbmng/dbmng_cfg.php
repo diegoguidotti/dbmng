a0da34a19b1e6f69bbfcd3863ec9a9bcf5ba8b3e
Michele Mammini <mamminim@gmail.com>
2013-08-01 12:40:52 +0200

    Add dbmng_cfg file deleted by mistake

---
+++ library/dbmng/dbmng_cfg.php
@@ -0,0 +1,18 @@
+<?php
+
+    if(!defined( 'DBMNG_CMS'))
+			{
+				define( 'DBMNG_CMS'          , 'drupal' );  // available value: 'drupal', 'none' ('joomla' will follows
+			}
+
+    if(!defined( 'DBMNG_DB'))
+			{
+				define( 'DBMNG_DB'           , 'mysql' );   // available value: 'mysql', 'postgres', 'pdo'
+			}
+
+		define( 'DBMNG_DB_HOST'      , 'localhost' );
+		define( 'DBMNG_DB_NAME'      , 'clima' );
+		define( 'DBMNG_DB_USER'      , 'admin' );
+		define( 'DBMNG_DB_PASS'      , '' );
+		define( 'DBMNG_DB_TBL_PREFIX', 'clima' );
+?>
