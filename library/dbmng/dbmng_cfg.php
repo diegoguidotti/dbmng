2619bccf0e0c8dd7246a3143420051236f302da6
diegoguidotti <diego.guidotti@gmail.com>
2013-08-03 07:08:35 -0700

    test svn

--- library/dbmng/dbmng_cfg.php
+++ library/dbmng/dbmng_cfg.php
@@ -13,6 +13,6 @@
 		define( 'DBMNG_DB_HOST'      , 'localhost' );
 		define( 'DBMNG_DB_NAME'      , 'clima' );
 		define( 'DBMNG_DB_USER'      , 'admin' );
-		define( 'DBMNG_DB_PASS'      , 'aaaa' );
+		define( 'DBMNG_DB_PASS'      , '' );
 		define( 'DBMNG_DB_TBL_PREFIX', 'clima' );
 ?>
