e5541224163c704e867deaece49c9d50dc8e12bf
Michele Mammini <mamminim@gmail.com>
2013-08-09 10:53:34 +0200

    _climasouth_workspace: added expert form

---
+++ tests/climasouth/c_tables_1.sql
@@ -0,0 +1,16 @@
+CREATE TABLE  c_expert (
+  id_c_expert int(11) NOT NULL AUTO_INCREMENT,
+  id_c_country int(11) NOT NULL,
+  name varchar(255,
+  jobtitle varchar(255),
+  institution varchar(255),
+  phone1 varchar(255),
+  phone2 varchar(255),
+  address varchar(255),
+  email varchar(255),
+  skype varchar(255),
+  twitter varchar(255),
+  PRIMARY KEY (id_c_expert)
+) ENGINE=MyISAM;
+
+
