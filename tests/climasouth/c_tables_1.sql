5c29720d3ccd76f0e883a29e34acdee61bb8ea3c
Michele Mammini <mamminim@gmail.com>
2013-08-09 16:07:12 +0200

    tables to be added

--- tests/climasouth/c_tables_1.sql
+++ tests/climasouth/c_tables_1.sql
@@ -1,7 +1,7 @@
 CREATE TABLE  c_expert (
   id_c_expert int(11) NOT NULL AUTO_INCREMENT,
   id_c_country int(11) NOT NULL,
-  name varchar(255,
+  name varchar(255),
   jobtitle varchar(255),
   institution varchar(255),
   phone1 varchar(255),
@@ -13,3 +13,19 @@ CREATE TABLE  c_expert (
   PRIMARY KEY (id_c_expert)
 ) ENGINE=MyISAM;
 
+CREATE TABLE  c_institution (
+  id_c_institution int(11) NOT NULL AUTO_INCREMENT,
+  id_c_country int(11) NOT NULL,
+  name_institution varchar(255),
+  phone1 varchar(255),
+  phone2 varchar(255),
+  address varchar(255),
+  email varchar(255),
+  PRIMARY KEY (id_c_institution)
+) ENGINE=MyISAM;
+
+CREATE TABLE  c_project (
+  id_c_project int(11) NOT NULL AUTO_INCREMENT,
+  name_project varchar(255),
+  PRIMARY KEY (id_c_project)
+) ENGINE=MyISAM;
