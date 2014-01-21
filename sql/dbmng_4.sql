bd4521bcd6572a980f91e1bf53d24f8a58617773
Michele Mammini <mamminim@gmail.com>
2014-01-21 16:48:07 +0100

    add view

--- sql/dbmng_4.sql
+++ sql/dbmng_4.sql
@@ -8,3 +8,7 @@ PRIMARY KEY (  id_dbmng_type_table )
 
 insert into dbmng_type_table values (1, 'Content table');
 insert into dbmng_type_table values (2, 'System table');
+
+--view
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `dbmng_tables_ext` AS select `t`.`id_table` AS `id_table`,`t`.`id_table_type` AS `id_table_type`,`t`.`table_name` AS `table_name`,`t`.`table_desc` AS `table_desc`,`t`.`table_label` AS `table_label`,count(0) AS `fld` from (`dbmng_tables` `t` left join `dbmng_fields` `f` on((`t`.`id_table` = `f`.`id_table`))) group by `t`.`id_table`,`t`.`id_table_type`,`t`.`table_name`,`t`.`table_desc`,`t`.`table_label`;
+
