a6b37a067fd4a1c82ec2ce21801b0b5be2af0bdb
Michele Mammini <mamminim@gmail.com>
2014-02-12 14:51:16 +0100

    fine tuning
    modify libs
    add the possibility to work in different environment

--- tests/testjs.php
+++ tests/testjs.php
@@ -9,10 +9,10 @@
 <script src="http://code.jquery.com/mobile/1.3.2/jquery.mobile-1.3.2.min.js"></script>
 -->
 
-<link rel="stylesheet" href="/libs/jquery.mobile-1.3.2.min.css" />
-<script src="/libs/jquery-1.9.1.min.js"></script>
-<script src="/libs/jquery.mobile-1.3.2.min.js"></script>
-<script src="/libs/jstorage.min.js"></script>
+<link rel="stylesheet" href="../libs/jquery.mobile-1.3.2.min.css" />
+<script src="../libs/jquery-1.9.1.min.js"></script>
+<script src="../libs/jquery.mobile-1.3.2.min.js"></script>
+<script src="../libs/jstorage.min.js"></script>
 
 <style type="text/css" media="all">
 	@import url("../library/dbmng/assets/dbmng.css");
@@ -20,11 +20,9 @@
 </head>
 <body>
 
-
 <div id="table2"></div>
 
 <?php 
-
   //we need to define some global variable to use dbmg independently by Drupal
 	define( 'DBMNG_LIB_PATH'    , '../library/' );
 	define( 'DBMNG_CMS'         , 'none' );
@@ -35,34 +33,33 @@
 	include(DBMNG_LIB_PATH.'dbmng/dbmng_standalone.php');
 
 	//get the array storing the table metadata from record 1 in table dbmng_tables
-	if( gethostname() == "Galveston" )
+	if( gethostname() == "Galveston" || $_SERVER["HTTP_HOST"] == "www.michelemammini.it" )
 		$id_table = 1;
+	else if( $_SERVER["HTTP_HOST"] == "www.climasouth.eu" )
+		$id_table = 20;
 	else
 		$id_table = 11;
-	
+
 	$aForm    = dbmng_get_form_array($id_table); 
+	//print_r($aForm);
+	//echo "<br/>" . DBMNG_LIB_PATH;
 	//echo dbmng_crud($aForm);
   echo  dbmng_crud_js($aForm, Array() );
-
 ?>  
 
 	
 <!-- <div id="paste_here" >Paste Here from Excell</div> -->
-
 <script type="text/javascript">
 	var db;
-
-
   jQuery(document).ready(function() {
-	
 		db  = new Dbmng(aForm, {
 			'div_element':'table2',   //div id containing the table
 			'ajax_url':'ajax.php',    //Where is locate the php with ajax function (relative to the current PHP file)
 			'inline':1                //Enable editing in the table without creating a new form
 		});  
 		db.start();
-
-	
+		
+		/**
 		jQuery('#paste_here').bind('paste', function (e) {
           var tab=(event.clipboardData.getData('text/plain'));
           var lines=(tab.split('\n'));
@@ -82,16 +79,9 @@
           html+="</table>";
           
           jQuery("#paste_here").html(html);
-          
-      });
-
-	
+		});
+		*/
 	});
-
-
-
-
 </script>
-
 </body>
 </html>
