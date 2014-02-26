b486b77dfa3ca262e943cc25a6de19feb23d7e55
Michele Mammini <mamminim@gmail.com>
2014-02-26 09:08:42 +0100

    code indentation

--- library/dbmng/assets/dbmng_mobile.js
+++ library/dbmng/assets/dbmng_mobile.js
@@ -1,13 +1,9 @@
 function init_mobile(){
-
 	debug('start mobile');
-
 	var uid = jQuery.jStorage.get("dbmng_user");
 	if(uid){
-      createTableList(uid);
+    createTableList(uid);
 	}
-	
-
 }
 
 
@@ -20,11 +16,9 @@ function createTableList(d){
 
 	var h=' <ul class="table_container" data-role="listview" >';
 	jQuery.each(d.table, function(k,v){
-
-		  //h+='<div   data-role="collapsible" data-collapsed="true">';
-      h+='<li id="table_'+v.id_table+'"><a href="#" onClick="showTable('+v.id_table+')">'+v.table_label+'</a></li>';
-      //h+='</div>';
-			
+	  //h+='<div   data-role="collapsible" data-collapsed="true">';
+	  h+='<li id="table_'+v.id_table+'"><a href="#" onClick="showTable('+v.id_table+')">'+v.table_label+'</a></li>';
+	  //h+='</div>';
 	});
 	h+="</ul>";
 
@@ -34,7 +28,6 @@ function createTableList(d){
 
 
 function showTable(id_table) {
-
 	//Check if exist an aForm in the jstorage
 	var db= new Dbmng(id_table, {
 			'div_element':'table_edit_container',   //div id containing the table
@@ -47,84 +40,67 @@ function showTable(id_table) {
 		db.start();	
 
 		jQuery.mobile.changePage("#table_edit");
-
-
 }
 
 function doLogin(){
-    var user_id=jQuery('#user_id').val();
-    var password=jQuery('#password').val();
-  	debug('doLogin');
+  var user_id=jQuery('#user_id').val();
+  var password=jQuery('#password').val();
+	debug('doLogin');
 
-    if(user_id!='' && password!=''){
-               
-        var call=base_call+'?do_login=on&name='+user_id+'&pass='+password;
-        
-        jQuery.ajax({
-            url: call,
-            success: function(data) {
-								console.log(data);
-                var d=eval(' ('+data+');');
-
-                if(d.login){  
-
-			            	jQuery.jStorage.set("dbmng_user", d);
-                    createTableList(d);
-                    
-                }
-                else{                    
-                    showMessageBox(d.msg);                    
-                }
-                
-            }
-        });
-        
-        
-    }
-    else{
-        showMessageBox('Please insert userID and password');
-        
-        //alert(call);
-        
-    }
-   
-   return false;
+  if(user_id!='' && password!=''){
+    var call=base_call+'?do_login=on&name='+user_id+'&pass='+password;
     
-    
+    jQuery.ajax({
+      url: call,
+      success: function(data) {
+				debug(data);
+        var d=eval(' ('+data+');');
+
+        if(d.login){  
+        	jQuery.jStorage.set("dbmng_user", d);
+          createTableList(d);
+        }
+        else{                    
+          showMessageBox(d.msg);                    
+        }
+          
+      }
+    });
+  }
+  else{
+	  showMessageBox('Please insert userID and password');
+	  
+	  //alert(call);
+  }
+ 
+ return false;
 }
-
-
-
 
 
 function doLogout(){
 	debug("doLogout");
-    var call=base_call+'?do_logout=on';
-    jQuery.ajax({
-        url: call,
-        success: function(data) {
-            var d=eval(' ('+data+');');
-            //console.log(data);
-            
-            if(!d.login){                                     
-								
-								jQuery.jStorage.deleteKey("dbmng_user");
-
-								jQuery("#login_message").html('');
-
-                jQuery('#login_form').show();
-                jQuery('#logout_form').hide();
-            }
-            else{                    
-                ;
-            }
-            
-        }
-    });
-       
-   return false;
-    
-    
+  var call=base_call+'?do_logout=on';
+  jQuery.ajax({
+    url: call,
+    success: function(data) {
+      var d=eval(' ('+data+');');
+      //console.log(data);
+      
+      if(!d.login){                                     
+				jQuery.jStorage.deleteKey("dbmng_user");
+	
+				jQuery("#login_message").html('');
+	
+	      jQuery('#login_form').show();
+	      jQuery('#logout_form').hide();
+      }
+      else{                    
+          ;
+      }
+    }
+  });
+     
+ return false;
 }
 
 function goToLogin(){
@@ -136,23 +112,23 @@ function goToLogin(){
 
 // Create a dialog and display it
 function showMessageBox (message) {
-    
-    //console.log(message);
-    
-    // Create it in memory
-    var dlg = $("<div />")
-        .attr("data-role", "dialog")
-        .attr("id", "dialog");
-    var content = $("<div />")
-        .attr("data-role", "content")
-        .append($("<span />").html(message));
-    content.append("<a href=\"javascript:$('.ui-dialog').dialog('close'); " +
-        "return false;\" data-role=\"button\" data-rel=\"back\">Close</a>");
-
-    dlg.append(content);
-
-    dlg.appendTo($.mobile.pageContainer);
-
-    // show the dialog programmatically
-    jQuery.mobile.changePage(dlg, {role: "dialog"});
+	
+	//console.log(message);
+	
+	// Create it in memory
+	var dlg = $("<div />")
+	    .attr("data-role", "dialog")
+	    .attr("id", "dialog");
+	var content = $("<div />")
+	    .attr("data-role", "content")
+	    .append($("<span />").html(message));
+	content.append("<a href=\"javascript:$('.ui-dialog').dialog('close'); " +
+	    "return false;\" data-role=\"button\" data-rel=\"back\">Close</a>");
+	
+	dlg.append(content);
+	
+	dlg.appendTo($.mobile.pageContainer);
+	
+	// show the dialog programmatically
+	jQuery.mobile.changePage(dlg, {role: "dialog"});
 }
