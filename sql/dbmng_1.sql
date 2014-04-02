3560902fbd1e059d6d244299fb29039c461f7839
diegoguidotti <diego.guidotti@gmail.com>
2014-04-02 18:11:41 +0200

    TEst

--- sql/dbmng_1.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE dbmng_fields ADD pk INT(1) DEFAULT 0;
-ALTER TABLE dbmng_fields ADD field_function varchar(100) DEFAULT NULL;
-
--- ALTER TABLE dbmng_fields ADD field_object varchar( 100 ) DEFAULT 0; -- associated object field
--- ALTER TABLE dbmng_fields ADD field_type   varchar( 1 ) DEFAULT 0; --referred to the object
--- ALTER TABLE dbmng_fields ADD pk INT( 1 ) DEFAULT 0;
